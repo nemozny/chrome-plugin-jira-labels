4e9b4cc054852c0278c2e4f55f8c8bb9e2e247bf
Pavel Helia <pavel.helia@siemens.com>
2022-06-28 16:04:31 +0200

    customer vs invisible; cleaner code

--- popup.js
+++ popup.js
@@ -1,29 +1,54 @@
-// Initialize butotn with users's prefered color
 let QA = document.getElementById("QA");
 let Doc = document.getElementById("Documentation");
 let Star = document.getElementById("Startest");
 let Large = document.getElementById("Large");
+let Small = document.getElementById("Small");
 
-// chrome.storage.sync.get("color", ({ color }) => {
-//   changeColor.style.backgroundColor = color;
-// });
+function updateTicket(payload) {
+  let jira = document.getElementsByName('ajs-issue-key')[0].getAttribute('content')
+  let url = document.getElementsByName('ajs-base-url')[0].getAttribute('content')
+  if (typeof(jira) == 'undefined' || typeof(url) == 'undefined') {
+    console.log("Not in Jira");
+    console.log(base_url);
+    console.log(url);
+    console.log(jira);
+    return;
+  }
 
-// When the button is clicked, inject setPageBackgroundColor into current page
-// changeColor.addEventListener("click", async () => {
-//   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  var xhr = new XMLHttpRequest();
+  let endpoint = url + "/rest/api/2/issue/" + jira
+  xhr.open("PUT", endpoint)
 
-//   chrome.scripting.executeScript({
-//     target: { tabId: tab.id },
-//     function: setPageBackgroundColor,
-//   });
-// });
+  xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+
+  xhr.onreadystatechange = function () { // Call a function when the state changes.
+    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+      // Request finished. Do processing here.
+      console.log(xhr.responseText);
+    }
+  }
+
+  body = {
+    "update": payload
+  };
+  json = JSON.stringify(body);
+  xhr.send(json);
+
+  window.location.reload();
+}
 
 QA.addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    function: addQA,
+    function: updateTicket,
+    args: [{
+      "labels" : 
+        [ 
+          { "add": "ManualTesting_NotRequired" }
+        ]
+    }]
   });
 });
 
@@ -32,7 +57,13 @@ Doc.addEventListener("click", async () => {
 
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    function: addDoc,
+    function: updateTicket,
+    args: [{
+      "labels" : 
+        [ 
+          { "add": "Documentation_NotRequired" }
+        ]
+    }]
   });
 });
 
@@ -41,7 +72,13 @@ Star.addEventListener("click", async () => {
 
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    function: addStar,
+    function: updateTicket,
+    args: [{
+      "labels" : 
+        [ 
+          { "add": "QA-AutomatedTest-Not-Required" }
+        ]
+    }]
   });
 });
 
@@ -50,214 +87,30 @@ Large.addEventListener("click", async () => {
 
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    function: addLarge,
+    function: updateTicket,
+    args: [{
+      "labels" : 
+        [ 
+          { "add": "Documentation_NotRequired" },
+          { "add": "QA-AutomatedTest-Not-Required" },
+          { "add": "ManualTesting_NotRequired" }
+        ]
+    }]
   });
 });
 
-// The body of this function will be execuetd as a content script inside the
-// current page
-// function setPageBackgroundColor() {
-//   chrome.storage.sync.get("color", ({ color }) => {
-//     document.body.style.backgroundColor = color;
-//   });
-// }
+Small.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-// function addLabels(label) {
-//   let jira = document.getElementsByName('ajs-issue-key')[0].getAttribute('content')
-//   let url = document.getElementsByName('ajs-base-url')[0].getAttribute('content')
-
-//   var xhr = new XMLHttpRequest();
-//   let endpoint = url + "/rest/api/2/issue/" + jira
-//   xhr.open("PUT", endpoint)//, true);
-
-//   //Send the proper header information along with the request
-//   // xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-//   xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");
-
-//   xhr.onreadystatechange = function () { // Call a function when the state changes.
-//     if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-//       // Request finished. Do processing here.
-//       console.log(xhr.responseText);
-//     }
-//   }
-
-//   body = {
-//     "update":
-//     {
-//       "labels":
-//         [
-//           { "add": label }
-//         ]
-//     }
-//   }
-//   json = JSON.stringify(body)
-//   xhr.send(json);
-
-//   // jira = document.getElementsByName('ajs-issue-key')[0].getAttribute('content')
-//   // console.log(jira)
-// }
-
-function addQA() {
-  let label = "ManualTesting_NotRequired"
-  function addLabels(label) {
-    let jira = document.getElementsByName('ajs-issue-key')[0].getAttribute('content')
-    let url = document.getElementsByName('ajs-base-url')[0].getAttribute('content')
-  
-    var xhr = new XMLHttpRequest();
-    let endpoint = url + "/rest/api/2/issue/" + jira
-    xhr.open("PUT", endpoint)//, true);
-  
-    //Send the proper header information along with the request
-    // xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");
-  
-    xhr.onreadystatechange = function () { // Call a function when the state changes.
-      if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-        // Request finished. Do processing here.
-        console.log(xhr.responseText);
-      }
-    }
-  
-    body = {
-      "update":
-      {
-        "labels":
-          [
-            { "add": label }
-          ]
-      }
-    }
-    json = JSON.stringify(body)
-    xhr.send(json);
-
-    window.location.reload();
-  
-    // jira = document.getElementsByName('ajs-issue-key')[0].getAttribute('content')
-    // console.log(jira)
-  }
-  addLabels(label)
-}
-
-function addStar() {
-  let label = "QA-AutomatedTest-Not-Required"
-  function addLabels(label) {
-    let jira = document.getElementsByName('ajs-issue-key')[0].getAttribute('content')
-    let url = document.getElementsByName('ajs-base-url')[0].getAttribute('content')
-  
-    var xhr = new XMLHttpRequest();
-    let endpoint = url + "/rest/api/2/issue/" + jira
-    xhr.open("PUT", endpoint)//, true);
-  
-    //Send the proper header information along with the request
-    // xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");
-  
-    xhr.onreadystatechange = function () { // Call a function when the state changes.
-      if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-        // Request finished. Do processing here.
-        console.log(xhr.responseText);
-      }
-    }
-  
-    body = {
-      "update":
-      {
-        "labels":
-          [
-            { "add": label }
-          ]
-      }
-    }
-    json = JSON.stringify(body)
-    xhr.send(json);
-
-    window.location.reload();
-  
-    // jira = document.getElementsByName('ajs-issue-key')[0].getAttribute('content')
-    // console.log(jira)
-  }
-  addLabels(label)
-}
-
-function addDoc() {
-  let label = "Documentation_NotRequired"
-  function addLabels(label) {
-    let jira = document.getElementsByName('ajs-issue-key')[0].getAttribute('content')
-    let url = document.getElementsByName('ajs-base-url')[0].getAttribute('content')
-  
-    var xhr = new XMLHttpRequest();
-    let endpoint = url + "/rest/api/2/issue/" + jira
-    xhr.open("PUT", endpoint)//, true);
-  
-    //Send the proper header information along with the request
-    // xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");
-  
-    xhr.onreadystatechange = function () { // Call a function when the state changes.
-      if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-        // Request finished. Do processing here.
-        console.log(xhr.responseText);
-      }
-    }
-  
-    body = {
-      "update":
-      {
-        "labels":
-          [
-            { "add": label }
-          ]
-      }
-    }
-    json = JSON.stringify(body)
-    xhr.send(json);
-
-    window.location.reload();
-  
-    // jira = document.getElementsByName('ajs-issue-key')[0].getAttribute('content')
-    // console.log(jira)
-  }
-  addLabels(label)
-}
-
-function addLarge() {
-  function addLabels() {
-    let jira = document.getElementsByName('ajs-issue-key')[0].getAttribute('content')
-    let url = document.getElementsByName('ajs-base-url')[0].getAttribute('content')
-  
-    var xhr = new XMLHttpRequest();
-    let endpoint = url + "/rest/api/2/issue/" + jira
-    xhr.open("PUT", endpoint)//, true);
-  
-    //Send the proper header information along with the request
-    // xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");
-  
-    xhr.onreadystatechange = function () { // Call a function when the state changes.
-      if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-        // Request finished. Do processing here.
-        console.log(xhr.responseText);
-      }
-    }
-  
-    body = {
-      "update":
-      {
-        "labels":
-          [
-            { "add": "Documentation_NotRequired" },
-            { "add": "QA-AutomatedTest-Not-Required" },
-            { "add": "ManualTesting_NotRequired" }
-          ]
-      }
-    }
-    json = JSON.stringify(body)
-    xhr.send(json);
-
-    window.location.reload();
-  
-    // jira = document.getElementsByName('ajs-issue-key')[0].getAttribute('content')
-    // console.log(jira)
-  }
-  addLabels()
-}
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: updateTicket,
+    args: [{
+      "labels" : 
+        [ 
+          { "add": "QA-AutomatedTest-Not-Required" },
+          { "add": "ManualTesting_NotRequired" }
+        ]
+    }]
+  });
+});
